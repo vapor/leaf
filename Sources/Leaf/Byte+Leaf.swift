37bdf88512bc79cb6573898ac3030844989469ba
Logan Wright <logan.william.wright@gmail.com>
2017-02-15 17:00:16 +0100

    clean up leaf specific bytes file

--- Sources/Leaf/Byte+Leaf.swift
+++ Sources/Leaf/Byte+Leaf.swift
@@ -1,15 +1,3 @@
-//// TODO: => Core
-//extension Byte {
-////    public static let openParenthesis = "(".makeBytes().first!
-////    public static let closedParenthesis = ")".makeBytes().first!
-//
-//    public static let openCurly = "{".makeBytes().first!
-//    public static let closedCurly = "}".makeBytes().first!
-//
-//    public static let quotationMark = "\"".makeBytes().first!
-//}
-
-// FIXME: Can this be rm?
 extension Sequence where Iterator.Element == Byte {
     public static var whitespace: Bytes {
         return [ .space, .newLine, .carriageReturn, .horizontalTab]
