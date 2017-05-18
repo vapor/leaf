6ed457f7fc539b0204be7705c55f98a75fa5ce91
Logan Wright <logan.william.wright@gmail.com>
2017-05-18 14:15:04 +0100

    package/readme

--- Package.swift
+++ Package.swift
@@ -1,11 +1,9 @@
 import PackageDescription
 
-let beta = Version(2,0,0, prereleaseIdentifiers: ["beta"])
-
 let package = Package(
     name: "Leaf",
     dependencies: [
-        .Package(url: "https://github.com/vapor/core.git", beta),
-        .Package(url: "https://github.com/vapor/node.git", beta),
+        .Package(url: "https://github.com/vapor/core.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/node.git", majorVersion: 2),
     ]
 )
