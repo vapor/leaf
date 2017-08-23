250b0353c9393e7097e09c98c5f5ee51802ff4d4
Tanner Nelson <me@tanner.xyz>
2017-08-22 23:56:37 -0400

    file reader updates

--- Sources/Leaf/Utilities/FileReader.swift
+++ Sources/Leaf/Utilities/FileReader.swift
@@ -22,36 +22,3 @@ extension Data {
     }
 }
 
-// FIXME: does this work?
-extension Array where Element: FutureType {
-    public var resolved: Future<[Element.Expectation]> {
-        let promise = Promise<[Element.Expectation]>()
-
-        var elements: [Element.Expectation] = []
-
-        var iterator = makeIterator()
-        func doit(_ future: Element) {
-            future.onComplete(asynchronously: nil) { element in
-                do {
-                    let res = try element.assertSuccess()
-                    elements.append(res)
-                    if let next = iterator.next() {
-                        doit(next)
-                    } else {
-                        promise.complete(elements)
-                    }
-                } catch {
-                    promise.complete(error)
-                }
-            }
-        }
-
-        if let first = iterator.next() {
-            doit(first)
-        } else {
-            promise.complete(elements)
-        }
-
-        return promise.future
-    }
-}
