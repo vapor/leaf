5deec87593af9919440472351306e90310c88869
Milan Vít <milanvit@milanvit.net>
2018-02-12 08:06:34 +0100

    Updated update of dependencies

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/async.git", .exact("1.0.0-beta.1")),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .package(url: "https://github.com/vapor/core.git", .exact("1.0.0-beta.1")),
+        .package(url: "https://github.com/vapor/core.git", .exact("3.0.0-beta.1")),
 
         // Service container and configuration system.
         .package(url: "https://github.com/vapor/service.git", .exact("1.0.0-beta.1")),
