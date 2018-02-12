91e708ee88acd5c8b8ee372798db568476e4ca60
Milan Vít <milanvit@milanvit.net>
2018-02-12 07:11:07 +0100

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -8,16 +8,16 @@ let package = Package(
     ],
     dependencies: [
         // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/async.git", .exact("1.0.0-beta.1")),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .package(url: "https://github.com/vapor/core.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/core.git", .exact("1.0.0-beta.1")),
 
         // Service container and configuration system.
-        .package(url: "https://github.com/vapor/service.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/service.git", .exact("1.0.0-beta.1")),
 
         // Easy-to-use foundation for building powerful templating languages in Swift.
-        .package(url: "https://github.com/vapor/template-kit.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/template-kit.git", .exact("1.0.0-beta.1")),
     ],
     targets: [
         .target(name: "Leaf", dependencies: ["Async", "Bits", "CodableKit", "COperatingSystem", "Service", "TemplateKit"]),
