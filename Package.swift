b4f384e15b5662e58e443f94d6d2adfb8d4924c1
Tanner Nelson <me@tanner.xyz>
2018-03-03 19:36:22 -0500

    update to NIO-based dependencies

--- Package.swift
+++ Package.swift
@@ -7,17 +7,14 @@ let package = Package(
         .library(name: "Leaf", targets: ["Leaf"]),
     ],
     dependencies: [
-        // ⏱ Promises and reactive-streams in Swift built for high-performance and scalability.
-        .package(url: "https://github.com/vapor/async.git", from: "1.0.0-rc"),
-
         // 🌎 Utility package containing tools for byte manipulation, Codable, OS APIs, and debugging.
-        .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/vapor/core.git", .branch("nio")),
 
         // 📦 Dependency injection / inversion of control framework.
-        .package(url: "https://github.com/vapor/service.git", from: "1.0.0-rc"),
+        .package(url: "https://github.com/vapor/service.git", .branch("nio")),
 
         // 📄 Easy-to-use foundation for building powerful templating languages in Swift.
-        .package(url: "https://github.com/vapor/template-kit.git", from: "1.0.0-rc"),
+        .package(url: "https://github.com/vapor/template-kit.git", .branch("nio")),
     ],
     targets: [
         .target(name: "Leaf", dependencies: ["Async", "Bits", "CodableKit", "COperatingSystem", "Service", "TemplateKit"]),
