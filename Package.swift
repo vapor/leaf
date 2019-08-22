a29552493519a1cfe2f2dce5f217b6679635c259
tanner0101 <me@tanner.xyz>
2019-08-22 16:11:15 -0400

    use leafkit tag

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "Leaf", targets: ["Leaf"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/leaf-kit.git", .branch("master")),
+        .package(url: "https://github.com/vapor/leaf-kit.git", from: "1.0.0-alpha"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-alpha.2"),
     ],
     targets: [
