6104096a71aaa2144f0bcb2b382ffbd529b9cec6
tanner0101 <me@tanner.xyz>
2019-12-09 15:33:19 -0500

    beta.2

--- Package.swift
+++ Package.swift
@@ -10,11 +10,11 @@ let package = Package(
         .library(name: "Leaf", targets: ["Leaf"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/leaf-kit.git", .branch("master")),
-        .package(url: "https://github.com/vapor/vapor.git", .branch("master")),
+        .package(url: "https://github.com/vapor/leaf-kit.git", from: "1.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.2"),
     ],
     targets: [
         .target(name: "Leaf", dependencies: ["LeafKit", "Vapor"]),
-        .testTarget(name: "LeafTests", dependencies: ["Leaf"]),
+        .testTarget(name: "LeafTests", dependencies: ["Leaf", "XCTVapor"]),
     ]
 )
