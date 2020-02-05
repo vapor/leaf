ef0312ca4b586659cc271ac83212050b974645bd
Adir Burke @ Work <zaraserver@dev.local>
2020-02-05 13:17:22 +1100

    Updated version of leaf-kit in Package.swift so it will build - Renamed property to be more inline with changes to leaf-kit

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "Leaf", targets: ["Leaf"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/leaf-kit.git", from: "1.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/leaf-kit.git", from:"1.0.0-beta.2.1"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.2"),
     ],
     targets: [
