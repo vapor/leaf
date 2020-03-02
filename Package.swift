a6490ee704c250f07c13a26429f35006a306a0e4
tanner0101 <me@tanner.xyz>
2020-03-02 11:47:20 -0500

    rcs

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .library(name: "Leaf", targets: ["Leaf"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/leaf-kit.git", from: "1.0.0-beta.2"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.4"),
+        .package(url: "https://github.com/vapor/leaf-kit.git", from: "1.0.0-rc.1"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc.1"),
     ],
     targets: [
         .target(name: "Leaf", dependencies: [
