a78c7919420f0fecf25bcc44b10a39154e6e3f46
Tim <0xtimc@gmail.com>
2021-08-07 16:58:36 +0100

    Export UnsafeUnescapedLeafTag

--- Sources/Leaf/Exports.swift
+++ Sources/Leaf/Exports.swift
@@ -1,4 +1,5 @@
 @_exported import protocol LeafKit.LeafTag
+@_exported import protocol LeafKit.UnsafeUnescapedLeafTag
 @_exported import struct LeafKit.LeafData
 @_exported import struct LeafKit.LeafContext
 @_exported import enum LeafKit.Syntax
