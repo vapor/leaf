dc173e34c310b06f5d2dedefeb928919f7f2ff41
Tim Condon <0xTim@users.noreply.github.com>
2021-08-08 12:27:53 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "Leaf", targets: ["Leaf"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/leaf-kit.git", from: "1.3.0"),
+        .package(url: "https://github.com/vapor/leaf-kit.git", from: "1.3.1"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
     ],
     targets: [
