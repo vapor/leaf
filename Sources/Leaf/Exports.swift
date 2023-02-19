96b9ac7bb3c693b98a1dc511ef64756e6795279b
Tim Condon <0xTim@users.noreply.github.com>
2023-02-19 16:12:33 +0000

    API Docs (#223)
    * Update CI
    
    * Update SPI info
    
    * Wrap exports
    
    * Update API docs workflow
    
    * Add DocC bundle

--- Sources/Leaf/Exports.swift
+++ Sources/Leaf/Exports.swift
@@ -1,5 +1,9 @@
+#if !BUILDING_DOCC
+
 @_exported import protocol LeafKit.LeafTag
 @_exported import protocol LeafKit.UnsafeUnescapedLeafTag
 @_exported import struct LeafKit.LeafData
 @_exported import struct LeafKit.LeafContext
 @_exported import enum LeafKit.Syntax
+
+#endif
