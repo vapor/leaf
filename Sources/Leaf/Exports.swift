28f5f9078e470ded0fac0dde2cd548417c18a577
Gwynne Raskind <gwynne@vapor.codes>
2023-08-11 03:51:10 -0500

    Fix Exports.swift

--- Sources/Leaf/Exports.swift
+++ Sources/Leaf/Exports.swift
@@ -1,4 +1,12 @@
-#if !BUILDING_DOCC
+#if swift(>=5.8)
+
+@_documentation(visibility: internal) @_exported import protocol LeafKit.LeafTag
+@_documentation(visibility: internal) @_exported import protocol LeafKit.UnsafeUnescapedLeafTag
+@_documentation(visibility: internal) @_exported import struct LeafKit.LeafData
+@_documentation(visibility: internal) @_exported import struct LeafKit.LeafContext
+@_documentation(visibility: internal) @_exported import enum LeafKit.Syntax
+
+#else
 
 @_exported import protocol LeafKit.LeafTag
 @_exported import protocol LeafKit.UnsafeUnescapedLeafTag
